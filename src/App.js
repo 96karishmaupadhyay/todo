f67c3bd955fdacb73f3504bd5791feb16fff413e
96karishmaupadhyay <karishmaupadhyay1996@gmail.com>
2025-01-04 12:27:42 +0530

    blank input handling code written

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-import { useState } from 'react';
+import { useState ,useEffect} from 'react';
 import Todo from './Todo';
 
 function App() {
@@ -11,10 +11,25 @@ function App() {
     setTodo(e.target.value)
   }
   const handlesubmit=()=>{
-    console.log(TodoList)
+    // console.log(TodoList)
+    if(todo.trim()===""){
+      alert("invalid todo");
+      return;
+    }
     setTodoList([...TodoList,todo])
      setTodo("")
   }
+  useEffect(() => {
+    const savedTodoList = JSON.parse(localStorage.getItem('todoList'));
+    if (savedTodoList) {
+      setTodoList(savedTodoList);
+    }
+  }, []);
+  useEffect(() => {
+    if (TodoList.length > 0) {
+      localStorage.setItem('todoList', JSON.stringify(TodoList));
+    }
+  }, [TodoList]);
   return (
     <div className="app">
      
